6ed6f0d6a5839e30344134432480b3ff1aa91f10
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-07-27 17:08:02 +0100

    Tweaks to make the script work for me
    * portable hashbang
    * set -e for early fail
    * curl is always installed on a mac

--- get-r-mac.sh
+++ get-r-mac.sh
@@ -1,16 +1,18 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+set -e
 
 # Download and extract the main Mac Resources directory
 # Requires xar and cpio, both installed in the Dockerfile
-mkdir r-mac
-wget https://cran.r-project.org/bin/macosx/R-3.5.1.pkg \
-  --output-document r-mac/latest_r.pkg
+mkdir -p r-mac
+curl -o r-mac/latest_r.pkg \
+     https://cran.r-project.org/bin/macosx/R-3.5.1.pkg
+
 cd r-mac
 xar -xf latest_r.pkg
 rm -r r-1.pkg Resources tcltk8.pkg texinfo5.pkg Distribution latest_r.pkg
 cat r.pkg/Payload | gunzip -dc | cpio -i
-mv R.framework/Versions/Current/Resources/* ../r-mac
-rm -r r.pkg R.framework  
+mv R.framework/Versions/Current/Resources/* .
+rm -r r.pkg R.framework
 
 # Remove unneccessary files TODO: What else
-rm -r doc tests 
+rm -r doc tests
