70732fade97026529c4084990eb8846b2ea181ef
Dirk Schumacher <mail@dirk-schumacher.net>
2018-08-31 14:48:30 +0200

    Add rstudio reference

--- shiny/app.R
+++ shiny/app.R
@@ -1,4 +1,5 @@
-#
+# This is the standard template for a signle file shiny app by RStudio
+
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
